f381206e816f09f311fefb2218bc21a270e4f748
Steve Traugott <stevegt@t7a.org>
2015-04-05 16:14:24 -0700

    catch one-off error exposed when hashing pm log

--- test/test_16_32_64.py
+++ test/test_16_32_64.py
@@ -3,65 +3,65 @@
 from ctypes import *
 import random
 
-
-import rabinpoly
-
-lib = rabinpoly.lib
+import rabinpoly as lib
 
 window_size = 32
 min_block_size = 16384
 avg_block_size = 32768
 max_block_size = 65536
+buf_size = 512*1024
+buf = create_string_buffer(buf_size)
 
-rp = lib.rabin_init(
-		window_size, avg_block_size, min_block_size, max_block_size)
-
+rp = lib.rabin_init(window_size, 
+			avg_block_size, min_block_size, max_block_size)
+rpc = rp.contents
 
 random.seed(42)
-buf_size = 512*1024
-print buf_size
-
-buf = create_string_buffer(buf_size)
 
 for i in range(buf_size):
 	buf[i] = chr(random.randrange(0,256))
 
-def rnext(start, size, tcount, tdone):
-	print start, size,
-	bs = cast(addressof(buf) + start, c_char_p)
-	rc = lib.rabin_in(rp, bs, size, 0)
-	assert rc == 0
-	rc = lib.rabin_out(rp)
-	assert rc == 1
-	count = rp.contents.frag_size
-	print count, 
-	assert count == tcount
-	done = rp.contents.block_done
-	print done, 
-	assert done == tdone
-	print
-	return count
+def Split(rp, buf, buf_size):
+	while True:
+		if rpc.state & lib.RABIN_IN:
+			print rpc.state
+			bs = cast(addressof(buf), c_char_p)
+			rc = lib.rabin_in(rp, bs, buf_size, 0)
+			assert rc == 1
+		if rpc.state & lib.RABIN_OUT:
+			rc = lib.rabin_out(rp)
+			assert rc == 1
+		if rpc.state & lib.PROCESS_FRAGMENT:
+			yield rpc.block_size
+		if rpc.state & lib.PROCESS_BLOCK:
+			pass
+		if rpc.state & lib.RABIN_RESET:
+			return;
 
-lengths = [
-		22708,
-		65536,
-		39502,
-		17057,
-		65536,
-		17295,
-		38959,
-		21301,
-		65536,
-		18033,
-		25136,
-		65536,
-		34529,
+refs = [
+		(22708, 1),
+		(65536, 1),
+		(39502, 1),
+		(17057, 1),
+		(65536, 1),
+		(17295, 1),
+		(38959, 1),
+		(21301, 1),
+		(65536, 1),
+		(18033, 1),
+		(25136, 1),
+		(65536, 1),
+		(34529, 1),
+		(27624, 0),
 	]
-start = 0
-for length in lengths:
-	count = rnext(start, buf_size-start, length, 1)
-	start += count
 
-rnext(start, buf_size-start, 27624, 0)
+fragments = Split(rp, buf, buf_size)
+for ref in refs:
+	length = ref[0]
+	block_done = ref[1]
+	size = fragments.next()
+	assert length == size
+	if block_done:
+		assert rpc.state & lib.PROCESS_BLOCK 
 
-lib.rabin_free(byref(rp))
+lib.rabin_free(rp)
